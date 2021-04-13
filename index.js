2fa14baa7b4f13c22154777bcaee13a378205d02
黄信越 <enter3017sky@gmail.com>
2021-04-14 01:16:37 +0800

    Update - test

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ async function updateGist(stats) {
     const line = [
       trimRightStr(name, 10).padEnd(10),
       time.padEnd(14),
-      generateBarChart(percent, 19),
+      generateBarChart(percent, 17),
       String(percent.toFixed(1)).padStart(5) + "%"
     ];
 
